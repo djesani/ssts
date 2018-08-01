0ebea673e7685c5e32afc120e7b0ede8ddecc31e
unknown <bhudiahiten@gmail.com>
2018-08-01 23:50:46 +1000

    Added all pages(static pages) with content

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,24 @@ router.get('/about-lordSwaminarayan', function(req, res, next) {
   res.render('about-lordSwaminarayan', { title: 'SSTS' });
 });
 
+router.get('/mandir-history', function(req, res, next) {
+  res.render('mandir-history', { title: 'SSTS' });
+});
+
+router.get('/acharyas', function(req, res, next) {
+  res.render('acharyas', { title: 'SSTS' });
+});
+
+router.get('/history', function(req, res, next) {
+  res.render('history', { title: 'SSTS' });
+});
+
+router.get('/donations', function(req, res, next) {
+  res.render('donations', { title: 'SSTS' });
+});
+
+router.get('/useful-links', function(req, res, next) {
+  res.render('useful-links', { title: 'SSTS' });
+});
+
 module.exports = router;
