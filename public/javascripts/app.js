cb5c9b6ca509d821aeacb5ef7feda516c7e90abf
hi10-b <41531756+hi10-b@users.noreply.github.com>
2019-02-24 23:09:38 +1100

    updated admin

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,11 +4,24 @@ element.addEventListener('submit', event => {
   event.preventDefault();
   console.log('Form submission cancelled.');
 
+var inval = $('#adminForm :input');
+console.log(inval);
 
+var value = $("#inval").val();
+console.log(value);
 
-  var values = $(this).serialize();
-  console.log(values);
+function showValues() {
+var fields = $( "adminForm:input" ).serializeArray();
+    $( "#inval" ).empty();
+    jQuery.each( fields, function( i, field ) {
+      $( "#inval" ).append( field.value + " " );
+    });
+
+}
+$( "select" ).change( showValues );
+showValues();
 });
+
 
 // $('#adminForm').submit(function() {
 //   $(this).children('input[type=submit]').prop('disabled', true)
