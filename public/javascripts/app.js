5fef494a7059ffefd329ef4d05d61a0485f6e4b1
hi10-b <41531756+hi10-b@users.noreply.github.com>
2019-03-07 15:18:40 +1100

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,26 +1,67 @@
-
 const element = document.querySelector('form');
 element.addEventListener('submit', event => {
+
   event.preventDefault();
   console.log('Form submission cancelled.');
 
-var inval = $('#adminForm :input');
-console.log(inval);
+  var inval = $('#adminForm :input');
+  console.log( inval);
 
-var value = $("#inval").val();
-console.log(value);
+  // var inval2 = inval[1].value;
+  // console.log("ival2 " + inval2);
 
-function showValues() {
-var fields = $( "adminForm:input" ).serializeArray();
-    $( "#inval" ).empty();
-    jQuery.each( fields, function( i, field ) {
-      $( "#inval" ).append( field.value + " " );
-    });
+  var inval3 = {};
+  for (var i = 0; i < inval.length-1; i++) {
+//    inval[i].value = inval3[i];
+    console.log("inval3 " + inval[i].value);
+  }
 
-}
-$( "select" ).change( showValues );
-showValues();
 });
+
+  // var aaa = $('#eventDescription').val();
+  // console.log("aaa " + aaa);
+  //
+  // var output = {};
+  // $("adminForm").each(function() {
+  //   if($(this).attr().length > 0) {
+  //     output($(this).attr()) = $(this).value();
+  //   }
+  // });
+  //
+  // console.log("output " + output);
+  //
+  // var returnArray = {};
+  // for (var i = 0; i < adminForm.length; i++){
+  //   returnArray[adminForm[i]['name']] = adminForm[i]['value'];
+  //   console.log(returnArray);
+  // }
+  // return returnArray;
+
+  // function showValues() {
+  //   var str = $( "adminForm:input" ).serialize();
+  //   $( "#results" ).text( str );
+  // }
+  // $( "input[type='button']" ).on( "click", showValues );
+  // $( "select" ).on( "change", showValues );
+  // showValues();
+
+
+  // var value = $("#inval").val();
+  // console.log($(this).serialize());
+
+
+  // function showValues() {
+  //   var fields = $( "adminForm:input" ).serializeArray();
+  //   $( "#results" ).empty();
+  //   jQuery.each( fields, function( i, field ) {
+  //     ( "#results" ).append( field.value + " " );
+  //   });
+  //
+  // }
+  // $( ":button" ).click( showValues );
+  // showValues();
+
+
 
 
 // $('#adminForm').submit(function() {
