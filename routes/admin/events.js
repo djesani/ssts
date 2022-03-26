8f77f5ed2f8c026b3125f70698ca6b221f15c3bf
Davendra Jesani <28943686+djesani@users.noreply.github.com>
2022-03-26 18:59:45 +1100

    parse dates for frontend

--- routes/admin/events.js
+++ routes/admin/events.js
@@ -7,6 +7,20 @@ const router = express.Router();
 const externalImagePath = "/images/events/";
 const externalImagePathRegex = "^\/images\/events\/";
 
+const getDateFromTimestamp = timestamp => {
+    const date = new Date(timestamp);
+    const dateString = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+    return dateString;
+}
+
+const getTimestampFromDate = (date, isStart) => {
+    const [ day, month, year ] = date.split('/');
+    const dateString = `${year}-${month}-${day}`;
+    const dateTime = isStart ? `${dateString} 00:00:00` : `${dateString} 23:59:00`;
+    const timestamp = new Date(dateTime).getTime();
+    return timestamp;
+}
+
 router.get('/', function(req, res, next) {
     const allEvents = [];
 
@@ -16,6 +30,8 @@ router.get('/', function(req, res, next) {
       files.forEach(file => {
         const eventJson = JSON.parse(fs.readFileSync(`${eventFilePath}/${file}`, 'utf8'));
         eventJson.filename = file;
+        eventJson.startDate = getDateFromTimestamp(eventJson.startDate);
+        eventJson.endDate = getDateFromTimestamp(eventJson.endDate);
         allEvents.push(eventJson);
       });
       console.log("Got all events:");
@@ -26,6 +42,9 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     const filename = req.body.name.replace(/[^A-Za-z0-9]/g,'');
+    const { startDate: payloadStart, endDate: payloadEnd } = req.body;
+    const startDate = getTimestampFromDate(payloadStart, true);
+    const endDate = getTimestampFromDate(payloadEnd);
 
     // check image has right filepath
     if(!req.body.imageurl.match(externalImagePathRegex)){
@@ -33,7 +52,13 @@ router.post('/', function(req, res, next) {
         req.body.imageurl = externalImagePath + req.body.imageurl;
     }
 
-    fs.writeFile(`${eventFilePath}/${filename}`, JSON.stringify(req.body, null, 2), function (err) {
+    const mergeData = {
+        ...req.body,
+        startDate,
+        endDate
+    };
+
+    fs.writeFile(`${eventFilePath}/${filename}`, JSON.stringify(mergeData, null, 2), function (err) {
         if (err) throw err;
         console.log(`Created new event: ${filename}`);
         res.send("Created a new event with filename: " + filename);
@@ -42,9 +67,17 @@ router.post('/', function(req, res, next) {
 
 router.patch('/:filename', function(req, res, next) {
     const { filename } = req.params;
+    const { startDate: payloadStart, endDate: payloadEnd } = req.body;
 
     const eventJson = JSON.parse(fs.readFileSync(`${eventFilePath}/${filename}`, 'utf8'));
-    const mergeData = Object.assign(eventJson, req.body);
+    const startDate = getTimestampFromDate(payloadStart, true);
+    const endDate = getTimestampFromDate(payloadEnd);
+    const mergeData = { 
+        ...eventJson,
+        ...req.body,
+        startDate,
+        endDate
+    };
 
     fs.writeFile(`${eventFilePath}/${filename}`, JSON.stringify(mergeData, null, 2), function (err) {
         if (err) throw err;
