0f1411bcd1a042bc4c20c96839f2bf5788662461
Davendra Jesani <28943686+djesani@users.noreply.github.com>
2022-04-02 17:47:56 +1100

    added calendar route to show entries in mandir tvs

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,10 @@ router.get('/contact-us', function(req, res, next) {
   res.render('contact-us', { title: 'Contact Us' });
 });
 
+router.get('/calendar', function(req, res, next) {
+  res.render('calendar', { title: 'Calendar' });
+});
+
 router.get('/test-upload', function(req, res, next) {
   res.render('admin/test-upload', { title: 'test-upload' });
 });
