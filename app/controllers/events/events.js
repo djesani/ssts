296e5779b9cafc6b7d07fd24002d600b2ce15ae5
Davendra Jesani <28943686+djesani@users.noreply.github.com>
2022-08-13 10:59:38 +1000

    refactor events and update donations link

---
+++ app/controllers/events/events.js
@@ -0,0 +1,117 @@
+const db = require('../../../lib/db');
+const fs = require('fs');
+const path = require('path');
+const { eventFilePath, imageBasePath } = require('../../../config');
+
+const compareByDate = (a,b) => {
+  if (a.startDate < b.startDate)
+    return 1;
+  if (a.startDate > b.startDate)
+    return -1;
+  return 0;
+}
+
+const getNoEventImg = () => {
+    const min=1;
+    const max=4;
+    console.log("Generating random number");
+    const random = Math.floor(Math.random() * (+max - +min)) + +min;
+    console.log("Random number generated: " + random);
+
+    const imageurl = `${imageBasePath}/images/no-events/${random}.png`;
+    const lowimageurl = '';
+    // const imageurl = `${imageBasePath}/events/no-events/${random}.png`;
+    // const lowimageurl = `${imageBasePath}/events/no-events/${random}-lowly.png`;
+    return { imageurl, lowimageurl };
+}
+
+const generateLowResImage = (event) => {
+    event.lowimageurl = event.imageurl.replace('.jpg', '-lowly.jpg');
+}
+
+const getEvents = async (req, res, next) => {
+    const eventArray = [];
+    const futureEvents = [];
+    const pastEvents = [];
+    const now = new Date();
+    const todayDate = new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime();
+    
+    let eventToday = {
+      "startDate": 946645200000,
+      "endDate": 946645200000,
+      "name": "Future event placeholder",
+      "isPlaceholder" : true
+    }
+
+    fs.readdir(eventFilePath, function(err, files) {
+      console.log(files);
+      if(err) console.log(err);
+      files.forEach(file => {
+        const eventJson = JSON.parse(fs.readFileSync(`${eventFilePath}/${file}`, 'utf8'));
+        eventJson.imageurl = imageBasePath + eventJson.imageurl;
+        eventArray.push(eventJson);
+      });
+
+      console.log("Today's date: " + todayDate);
+      eventArray.sort(compareByDate).forEach(event => {
+        console.log("Event startDate: " + event.startDate);
+        console.log("Event publishDate: " + event.publishDate);
+        generateLowResImage(event);
+
+        const publishEvent = event.publishDate == undefined || event.publishDate <= todayDate ? true : false;
+
+        if(publishEvent && !event.unpublished){
+            if(event.startDate > todayDate){
+              console.log("A future event exists, adding to future list.");
+              futureEvents.push(event);
+            }else if(event.startDate === todayDate || (event.startDate <= todayDate && event.endDate >= todayDate)){
+              console.log("An event is on today!");
+              eventToday = event;
+              eventToday['isEventToday'] = true;
+            }else{
+              pastEvents.push(event);
+            }
+        }else{
+            console.log(`Event: ${event.name} NOT PUBLISHED. Publish date in future or event marked as Unpublished!`);
+        }
+      });
+
+      console.log(`Future events: ${futureEvents.length}`);
+
+      // Reverse order for future events only
+      if(futureEvents.length > 0){
+          futureEvents.reverse();
+      }
+
+      if(futureEvents.length > 0 || eventToday.isEventToday){
+        const maxEvents = eventToday.isEventToday === true ? 3 : 4;
+        console.log(`Future events: ${futureEvents.length}. We need to add placeholder to fill the row!`);
+
+        for (let i = futureEvents.length; i < maxEvents; i++){
+          console.log("Adding placeholderEvent to future events");
+          const { imageurl, lowimageurl } = getNoEventImg();
+          var newFutureEvent = {
+            "startDate": 2524568400000,
+            "endDate": 2524568400000,
+            "name": "Future event placeholder",
+            "imageurl": imageurl,
+            "lowimageurl": lowimageurl,
+            "isPlaceholder" : true
+          };
+          futureEvents.push(newFutureEvent);
+        }
+      }
+
+      console.log("Future events:");
+      console.log(futureEvents);
+      console.log("Past events:");
+      console.log(pastEvents);
+
+      console.log("DB not enabled. Returning events from file system");
+      res.render('events', { title: 'SSTS', futureEvents, pastEvents, eventToday });
+    });
+};
+
+module.exports = {
+    getEvents
+}
