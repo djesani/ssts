1c54e65a3913abad5a95c5804b5094978b18ceac
mchesh <mahesh.jeshani@gmail.com>
2024-02-02 20:14:36 +1100

    created firebase db

---
+++ sourceCode/kirtanavalli-firebase/src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCl5o4Rr0YNBAfjsQbhfyBuhMkQk_N6zT0",
+    authDomain: "kirtans-test.firebaseapp.com",
+    projectId: "kirtans-test",
+    storageBucket: "kirtans-test.appspot.com",
+    messagingSenderId: "767492193333",
+    appId: "1:767492193333:web:431a1dc9556d357166057c"
+  };
+
+const app = initializeApp(firebaseConfig);
