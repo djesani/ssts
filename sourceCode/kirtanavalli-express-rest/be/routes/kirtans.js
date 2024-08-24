df244ec6f430ca8499ad6b9bfdd730e3336125c4
mchesh <mahesh.jeshani@gmail.com>
2024-08-24 18:24:00 +1000

    c50

--- sourceCode/kirtanavalli-express-rest/be/routes/kirtans.js
+++ sourceCode/kirtanavalli-express-rest/be/routes/kirtans.js
@@ -2,54 +2,54 @@ const express = require("express");
 const db = require("../lib/postgres.db.js");
 const kirtans = express.Router();
 
-// kirtans.get("/search/", async (req, res) => {
-//   const sql = `
-//   SELECT 
-//     id,
-//     title_english
-//   FROM 
-//     kirtans
-//   WHERE
-//     description_lines_synced = 1
-//   ORDER BY 
-//     title_english 
-//   ASC
-//   LIMIT 100
-//   `;
-//   db.query(sql, [], (err, data) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: "success",
-//       data: data.rows
-//     });
-//   });
-// });
+kirtans.get("/search/", async (req, res) => {
+  const sql = `
+  SELECT 
+    id,
+    title_english
+  FROM 
+    kirtans
+  WHERE
+    description_lines_synced = 1
+  ORDER BY 
+    title_english 
+  ASC
+  LIMIT 100
+  `;
+  db.query(sql, [], (err, data) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: data.rows
+    });
+  });
+});
 
-// kirtans.get("/search/:term", async (req, res) => {
-//   const sql = `
-//   SELECT 
-//     title_english
-//   FROM 
-//     kirtans
-//   WHERE
-//     description_lines_synced = 1
-//   AND
-//     LOWER(title_english) LIKE '${req.params.term.toLowerCase()}%' 
-//   `;
-//   db.query(sql, [], (err, data) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: "success",
-//       data: data.rows
-//     });
-//   });
-// });
+kirtans.get("/search/:term", async (req, res) => {
+  const sql = `
+  SELECT 
+    title_english
+  FROM 
+    kirtans
+  WHERE
+    description_lines_synced = 1
+  AND
+    LOWER(title_english) LIKE '${req.params.term.toLowerCase()}%' 
+  `;
+  db.query(sql, [], (err, data) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: data.rows
+    });
+  });
+});
 
 kirtans.get("/:id", async (req, res) => {
 
@@ -64,6 +64,27 @@ kirtans.get("/:id", async (req, res) => {
     kirtans
   WHERE
     id = ${req.params.id}
+  `;
+
+  db.query(sql, [], (err, data) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: data.rows
+    });
+  });
+});
+
+kirtans.get("/categories/", async (req, res) => {
+
+  const sql = `
+  SELECT 
+    *
+  FROM 
+    categories
   `;
 
   db.query(sql, [], (err, data) => {
